c63b4860f7776ce068b8abe134fb16827beb3f46
Christian Kjær Laustsen <christianlaustsen@gmail.com>
2012-12-09 00:50:51 +0100

    reply the path.

--- src/commands/git/git.py
+++ src/commands/git/git.py
@@ -25,6 +25,7 @@ class Git(object):
         try:
             cmd = ["git", "clone", args[0]]
             path = ' '.join(args[1:])
+            self.commandInstance.replyWithMessage(args)
             self.commandInstance.replyWithMessage(path)
             #p = subprocess.Popen(cmd, cwd=path, stdout=subprocess.PIPE)
             #self.commandInstance.replyWithMessage(p.stdout.read())
