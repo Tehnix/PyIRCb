c924bbfa4ed28d4f3862876fadb8ecbc48a66a3b
Christian Kjær Laustsen <christianlaustsen@gmail.com>
2012-12-09 00:45:37 +0100

    test the path.

--- src/commands/git/git.py
+++ src/commands/git/git.py
@@ -25,6 +25,7 @@ class Git(object):
         try:
             cmd = ["git", "clone", args[0]]
             path = ' '.join(args[1:])
+            print(path)
             p = subprocess.Popen(cmd, cwd=path, stdout=subprocess.PIPE)
             self.commandInstance.replyWithMessage(p.stdout.read())
         except IndexError:
