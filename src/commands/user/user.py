244e38fe613e5635fc774f62680dd459847cff9d
Foxboron <mcfoxax@gmail.com>
2012-12-09 03:14:22 +0100

    Added user module

--- src/commands/user/user.py
+++ src/commands/user/user.py
@@ -1,18 +1,15 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """
-User command...
+Test command...
 
 """
 
-import grp
+from src.irc.database import Database
 
-
-class User(object):
-    """Something, something, something darkside...."""
-
+class user(object):
     def __init__(self, settingsInstance, commandInstance, cmdName, *args):
-        super(User, self).__init__()
+        super(Test, self).__init__()
         self.settingsInstance = settingsInstance
         self.commandInstance = commandInstance
         if cmdName is not None:
@@ -20,23 +17,46 @@ class User(object):
                 getattr(self, cmdName)(*args)
             else:
                 getattr(self, cmdName)()
+        self.db = Database(dbtype="SQLite", dbname="database.db")
+        self.db.execute(sql="""CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY,nickname TEXT, password TEXT, server TEXT)""")
+        self.db.execute(sql="""CREATE TABLE IF NOT EXISTS projects (id INTEGER PRIMARY KEY,userId INTEGER, name TEXT, dir TEXT)""")
     
-    def testing(self):
-        self.commandInstance.replyWithMessage(':D !')
+    def add(self, *args):
+        """$user.add (user password server)"""
+        data = {"nickname": args[0],
+                "password": args[1],
+                "server": args[2]}
+        self.db.insert(table="users", data=data)
+            
+    def rmrf(self, *args):
+        """$user.rmrf (user)
+        INB4 worst idea ever."""
+        data = {"nickname": args[0]}
+        self.db.delete(table="users", filters=data)
+        
+    def rmrfProject(self, *args):
+        """$user.rmrf (user)
+        INB4 worst idea ever."""
+        data = {"name": args[0]}
+        self.db.delete(table="projects", filters=data)
+    
+    def addProject(self, *args):
+        """$user.addProject (user projectName path)"""
+        user, projectName, path = args
+        search = {"nickname": user}
+        res = self.db.fetchone(table="users", filter=search)
+        userid = res[0]
+        data = {"userID": userid, "name": projectName, "dir": path}
+        self.db.insert(table="projects", filter=data)
 
-    def say(self, *args):
-        self.commandInstance.replyWithMessage(' '.join(args))
+    def printUser(self, *args):
+        """$user.printUser (user)"""
+        search = {"nickname": args[0]}
+        res = self.db.fetchone(table="users", filter=search)
+        self.commandInstance.replyWithMessage(res)
 
-    def users(self):
-        """Something"""
-        self.commandInstance.replyWithMessage(self._users())
-
-    def _users(self, output='string'):
-        for group in grp.getgrall():
-            if group.gr_name == 'users':
-                members = group.gr_mem
-        if output == 'list':
-            return members
-        else:
-            return ', '.join(members)
-
+    def printProject(self, *args):
+        """$user.printProject (project)"""
+        search = {"name": args[0]}
+        res = self.db.fetchone(table="projects", filter=search)
+        self.commandInstance.replyWithMessage(res)
